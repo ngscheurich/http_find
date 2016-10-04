12b667963f3f338e9b2eb67ca1f168ac04aa81cc
Nicholas Scheurich <nick@scheurich.me>
2016-10-04 11:02:12 -0500

    Handle URI args with no explicit scheme

--- lib/http_search/response.rb
+++ lib/http_search/response.rb
@@ -6,7 +6,8 @@ module HttpSearch
     attr_reader :body
 
     def initialize(uri)
-      party = HTTParty.get(uri)
+      furi = uri.start_with?("http") ? uri : "http://#{uri}"
+      party = HTTParty.get(furi)
       @code = party.response.code.to_i
       @body = @code == 200 ? party.response.body : nil
     end
