02d882b20262575168c25d0c5459dfb5f1c47c9d
Nicholas Scheurich <nick@scheurich.me>
2016-10-04 14:10:07 -0500

    Remove dependency on Thor

--- lib/http_find/cli.rb
+++ lib/http_find/cli.rb
@@ -1,15 +1,38 @@
-require "thor"
+require "optparse"
 
 module HttpFind
-  class Cli < Thor
-    option :regexp, type: :boolean, default: false, aliases: "-e",
-                    desc: "Indicate that TERM is a regular expression"
-    desc "find URI TERM", "searches the HTTP GET response of URI for TERM"
+  class Cli
+    def initialize
+      @options = {}
+      gather_opts
+    end
+
     def find(uri, term)
-      subject = options[:regexp] ? /#{term}/ : term
+      subject = @options[:regexp] ? /#{term}/ : term
       HttpFind.find(uri, subject).each { |l| puts l }
     end
 
-    default_task :find
+    private
+
+    def gather_opts
+      OptionParser.new do |opts|
+        opts.banner = "Usage: httpfind [options] URI TERM"
+
+        opts.on("-e", "--regexp",
+                "Indicate that TERM is a regular expression") do |e|
+          options[:regexp] = e
+        end
+
+        opts.on("-v", "--version", "Show version info") do
+          puts "httpfind #{HttpFind::VERSION}"
+          exit
+        end
+
+        opts.on("-h", "--help", "Display this message") do
+          puts opts
+          exit
+        end
+      end.parse!
+    end
   end
 end
