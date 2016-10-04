75acd5ead4205f2b0033e40bbc18182f404cf27d
Nicholas Scheurich <nick@scheurich.me>
2016-10-04 10:57:15 -0500

    Implement CLI

--- lib/http_search.rb
+++ lib/http_search.rb
@@ -2,6 +2,13 @@ require "http_search/version"
 require "http_search/response"
 require "http_search/matcher"
 require "http_search/output"
+require "http_search/cli"
 
 module HttpSearch
+  def self.find(uri, subject)
+    response = HttpSearch::Response.new(uri)
+    matcher = HttpSearch::Matcher.new(response.body)
+    output = HttpSearch::Output.new(matcher.match(subject))
+    output.lines
+  end
 end
--- test/http_search_test.rb
+++ test/http_search_test.rb
@@ -4,4 +4,17 @@ class HttpSearchTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::HttpSearch::VERSION
   end
+
+  def test_that_it_finds_in_source
+    stub_request(:get, "www.example.org").
+      to_return(status: 200, body: "Hello\nYellow")
+
+    expected = [
+      "1 | H\e[0;33;49mello\e[0m",
+      "2 | Y\e[0;33;49mello\e[0mw"
+    ]
+    result = HttpSearch.find("http://www.example.org", "ello")
+
+    assert_equal expected, result
+  end
 end
